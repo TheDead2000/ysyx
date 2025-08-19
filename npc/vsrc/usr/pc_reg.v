0335d79cf92f1c94e392fd946bc3d198fa68d5b2
tracer-ysyx <tracer@ysyx.org>
2025-08-18 23:16:35 -0700

    > sim RTL ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:16:35 up  6:25,  1 user,  load average: 1.32, 0.86, 0.87

--- npc/vsrc/usr/pc_reg.v
+++ npc/vsrc/usr/pc_reg.v
@@ -45,6 +45,7 @@ module pc_reg (
     end else if (bpu_pc_valid_i) begin : bpu_pc
       _pc_next = bpu_pc_i;
     end else begin
+       $display("pc_reg: pc_temp_plus4 = %h", pc_temp_plus4);
       _pc_next = pc_temp_plus4;
     end
   end
