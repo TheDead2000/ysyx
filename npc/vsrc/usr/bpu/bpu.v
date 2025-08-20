231b84be3845c10a924e2918a0c5acbb549f1e6a
tracer-ysyx <tracer@ysyx.org>
2025-08-20 06:10:18 -0700

    > sim RTL ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  06:10:18 up 10:49,  1 user,  load average: 1.65, 1.00, 0.61

--- npc/vsrc/usr/bpu/bpu.v
+++ npc/vsrc/usr/bpu/bpu.v
@@ -185,7 +185,7 @@ always @(posedge clk or posedge rst) begin
         // 当有新的弹出操作时，更新前递的数据
         if (ex_branch_valid_i && ex_branch_taken_i && ex_is_ret && !ex_stall_valid_i && ras_pop_pending) begin
             ras_pop_data <= id_ras_push_data_i;
-            $display("[RAS] POP UPDATED: data=0x%h", id_ras_push_data_i);
+            $display("[RAS] POP UPDATED: data=0x%h", ras[ras_sp-1]);
         end
     end
 end
@@ -358,18 +358,18 @@ wire ex_is_ret = (ex_inst_i[6:0] == 7'b1100111) &&
             // 处理RET指令（优先使用RAS）
             if (is_ret) begin
                 pdt_res = 1'b1; // RET总是跳转
-        //         if (ex_is_ret && !ex_stall_valid_i) begin
-        //  pdt_pc = ras[ras_sp-1];
-        // pred_used_ras = 1;
-        //       $display("[RAS] COMBO POP: target=0x%h", pdt_pc);
-        //     end
-        // else
-        //          if (ras_held_valid) begin
-        //         pdt_pc = ras_held_data;
-        //          pred_used_ras = 0;
-        //         $display("[RAS] USE HELD DATA: target=0x%h", ras_held_data);
-        //          end 
-        //         else
+                if (ex_is_ret && !ex_stall_valid_i) begin
+         pdt_pc = ras[ras_sp-1];
+        pred_used_ras = 1;
+              $display("[RAS] COMBO POP: target=0x%h", pdt_pc);
+            end
+        else
+                 if (ras_held_valid) begin
+                pdt_pc = ras_held_data;
+                 pred_used_ras = 0;
+                $display("[RAS] USE HELD DATA: target=0x%h", ras_held_data);
+                 end 
+                else
                 if (ras_pop_valid) begin
                 pdt_pc = ras_pop_data;
                 pred_used_ras = 0;
