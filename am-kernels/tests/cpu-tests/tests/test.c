2585ba8dfa1e350c2d0d819bb0e316eb0d5a8bb2
tracer-ysyx <tracer@ysyx.org>
2025-08-21 20:54:29 -0700

    > sim RTL ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:54:29 up 44 min,  1 user,  load average: 0.27, 1.62, 4.06

--- am-kernels/tests/cpu-tests/tests/test.c
+++ am-kernels/tests/cpu-tests/tests/test.c
@@ -1,16 +1,22 @@
-int test1()
+int test1(int a)
 {
+    a = 2;
     return 0;
 }
-int test2(){
+int test2(int b){
+    b = 3;
     return 0;
 } 
-int test3() {
+int test3(int c) {
+    c = 4;
     return 0;
 }
 int main() {
-  test1();
-  test2();
-  test3();
+    int a = 1;
+    int b;
+    int c;
+  test1(a);
+  test2(b);
+  test3(c);
   return 0;
 }
