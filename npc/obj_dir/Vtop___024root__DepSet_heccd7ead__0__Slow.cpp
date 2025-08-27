d17c06a53d8b6251bb2a5920520bbdab91200374
tracer-ysyx <tracer@ysyx.org>
2025-08-26 19:25:55 -0700

    > sim RTL ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:25:55 up  3:46,  1 user,  load average: 2.04, 1.49, 0.84

--- npc/obj_dir/Vtop___024root__DepSet_heccd7ead__0__Slow.cpp
+++ npc/obj_dir/Vtop___024root__DepSet_heccd7ead__0__Slow.cpp
@@ -1968,6 +1968,13 @@ VL_ATTR_COLD void Vtop___024root___stl_sequent__TOP__0(Vtop___024root* vlSelf) {
                                                        | (0x7feU 
                                                           & (vlSelf->top__DOT__u_icache_top__DOT__icache_final_data 
                                                              >> 0x14U)))))));
+        } else if ((0x67U == (0x7fU & vlSelf->top__DOT__u_icache_top__DOT__icache_final_data))) {
+            vlSelf->top__DOT__pdt_res = 1U;
+            if (vlSelf->top__DOT__ifu__DOT__bpu__DOT__btb_hit) {
+                vlSelf->top__DOT__bpu_pc_o = vlSelf->top__DOT__ifu__DOT__bpu__DOT__btb_target_val;
+            } else {
+                vlSelf->top__DOT__pdt_res = 0U;
+            }
         } else {
             if (vlSelf->top__DOT__ifu__DOT__bpu__DOT__t1_match) {
                 vlSelf->top__DOT__pdt_res = (1U & (
