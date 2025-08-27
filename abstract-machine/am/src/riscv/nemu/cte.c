04e543454fd7ed8805f628a0fa8e99383ffa8b7c
tracer-ysyx <tracer@ysyx.org>
2025-08-27 07:23:42 -0700

    > compile NEMU ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  07:23:42 up  1:50,  1 user,  load average: 0.25, 0.43, 0.31

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -44,11 +44,10 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 }
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-  Context *c  = (Context *)(kstack.end - sizeof(Context));
-  c->mepc = (uintptr_t)entry;
-  c->mstatus = 0x1800;
-  c->gpr[10] = (uintptr_t)arg; //a0
-  return c; 
+    Context* ctp = (Context*)kstack.end - 1; // 开辟空间
+    ctp->gpr[10] = (uintptr_t)arg;  // 传参到 a0
+    ctp->mepc = (uintptr_t)entry;
+    return ctp;
 
 }
 
