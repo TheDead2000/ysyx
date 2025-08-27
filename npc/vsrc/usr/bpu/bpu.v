9a33420a09c83527f6727725cd5b7d5d63135b5c
tracer-ysyx <tracer@ysyx.org>
2025-08-26 19:29:16 -0700

    > sim RTL ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:29:16 up  3:49,  1 user,  load average: 3.04, 2.11, 1.21

--- npc/vsrc/usr/bpu/bpu.v
+++ npc/vsrc/usr/bpu/bpu.v
@@ -373,15 +373,15 @@ assign ex_next_ras_top = (ex_next_ras_sp > 0) ? ras[ex_next_ras_sp - 1] : {`XLEN
                     pdt_pc = if_pc + {{12{if_inst[31]}}, if_inst[19:12], if_inst[20], if_inst[30:21], 1'b0};
                 end
             end
-            else   
-            if (is_jalr) begin
-                pdt_res = 1;
-                if (btb_hit) begin
-                    pdt_pc = btb_target_val;
-                end
-                else 
-                    pdt_res = 0;
-                end
+            // else   
+            // if (is_jalr) begin
+            //     pdt_res = 1;
+            //     if (btb_hit) begin
+            //         pdt_pc = btb_target_val;
+            //     end
+            //     else 
+            //         pdt_res = 0;
+            //     end
             // 处理分支指令
             else
              begin
