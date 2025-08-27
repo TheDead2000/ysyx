f425beb0a57ff4d9389ebd650e933837ee0aea6d
tracer-ysyx <tracer@ysyx.org>
2025-08-27 02:47:00 -0700

    > compile NEMU ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  02:47:00 up 43 min,  1 user,  load average: 0.38, 0.11, 0.19

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -4,30 +4,22 @@
 
 static Context* (*user_handler)(Event, Context*) = NULL;
 
-
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
+    // printf("c->mcause=%d\n",c->mcause);
+    int mcause = c->mcause;
+    mcause = (mcause >= 0 && mcause <= 19) ? 1 : mcause;
     Event ev = {0};
-    // printf("c->mcause %d\n",c->mcause);
-    switch (c->mcause) {
-      case -1: ev.event = EVENT_YIELD; break;   // yield
-      // case  0: ev.event = EVENT_SYSCALL; break; // exit
-      // case  1: ev.event = EVENT_SYSCALL; break; // yield
-      // case  2: ev.event = EVENT_SYSCALL; break; // open
-      // case  3: ev.event = EVENT_SYSCALL; break; // read
-      // case  4: ev.event = EVENT_SYSCALL; break; // write
-      // case  7: ev.event = EVENT_SYSCALL; break; // close
-      // case  8: ev.event = EVENT_SYSCALL; break; // lseek
-      // case  9: ev.event = EVENT_SYSCALL; break; // brk
-      case 11: ev.event = EVENT_YIELD; break;
-      // case 19: ev.event = EVENT_SYSCALL; break; // gettimeofday
-      default: ev.event = EVENT_SYSCALL; break;
+    switch (mcause) {
+      case 1: ev.event = EVENT_SYSCALL; break;
+      case -1: ev.event = EVENT_YIELD; break;
+      default: ev.event = EVENT_ERROR; break;
     }
 
     c = user_handler(ev, c);
     assert(c != NULL);
   }
-  c->mepc = c->mepc + 4;
+
   return c;
 }
 
@@ -44,19 +36,18 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 }
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-  Context *c  = (Context *)(kstack.end - sizeof(Context));
-  c->mepc = (uintptr_t)entry;
+  Context *c = (Context *)kstack.end - sizeof(Context);
+  c->mepc = (uintptr_t)entry; 
+  c->gpr[10] = (uintptr_t) arg;
   c->mstatus = 0x1800;
-  c->gpr[10] = (uintptr_t)arg; //a0
-  return c; 
-
+  return c;
 }
 
 void yield() {
 #ifdef __riscv_e
   asm volatile("li a5, -1; ecall");
 #else
-  asm volatile("li a7, 11; ecall");
+  asm volatile("li a7, -1; ecall");
 #endif
 }
 
