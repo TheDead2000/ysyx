e140fcf4e4d4e9e067552947edaa1361cde29b71
tracer-ysyx <tracer@ysyx.org>
2025-08-26 20:25:28 -0700

    > compile NEMU ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:25:28 up  4:45,  1 user,  load average: 1.64, 0.98, 0.65

--- am-kernels/kernels/yield-os/yield-os.c
+++ am-kernels/kernels/yield-os/yield-os.c
@@ -9,6 +9,14 @@ typedef union {
 static PCB pcb[2], pcb_boot, *current = &pcb_boot;
 
 static void f(void *arg) {
+  if((char*)arg == 1)
+  {
+    putch("A");
+  }
+  else
+  {
+    putch("B");
+  }
   while (1) {
     putch("?AB"[(uintptr_t)arg > 2 ? 0 : (uintptr_t)arg]);
     for (int volatile i = 0; i < 100000; i++) ;
@@ -24,8 +32,8 @@ static Context *schedule(Event ev, Context *prev) {
 
 int main() {
   cte_init(schedule);
-  pcb[0].cp = kcontext((Area) { pcb[0].stack, &pcb[0] + 1 }, f, "A1");
-  pcb[1].cp = kcontext((Area) { pcb[1].stack, &pcb[1] + 1 }, f, "B2");
+  pcb[0].cp = kcontext((Area) { pcb[0].stack, &pcb[0] + 1 }, f, (void *)1L);
+  pcb[1].cp = kcontext((Area) { pcb[1].stack, &pcb[1] + 1 }, f, (void *)2L);
   yield();
   panic("Should not reach here!");
 }
