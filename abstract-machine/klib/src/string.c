e53d00ec222fd2455f1058b63a88a884f17f84dd
tracer-ysyx <tracer@ysyx.org>
2025-08-27 20:10:23 -0700

    > compile NEMU ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:10:23 up 42 min,  1 user,  load average: 1.61, 1.95, 1.53

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -1,132 +1,107 @@
-#include <klib.h>
 #include <klib-macros.h>
+#include <klib.h>
 #include <stdint.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-size_t strlen(const char *s) {
+size_t strlen(const char *s)
+{
   size_t len = 0;
-  while (*s++ != '\0') {
+  while (*s++)
     len++;
-  }
   return len;
 }
 
-char *strcpy(char *dst, const char *src) {
-  char *d = dst;
-  
-  while ((*d++ = *src++) != 0);
+char *strcpy(char *dst, const char *src)
+{
+  assert(dst != NULL);
+  char *tmp = dst;
+  while (*src)
+    *dst++ = *src++;
+  *dst = '\0';
+  return tmp;
+}
 
+char *strncpy(char *dst, const char *src, size_t n)
+{
+  assert(dst != NULL);
+  char *tmp = dst;
+  while (*src && n--)
+    *dst++ = *src++;
+  *dst = '\0';
+  return tmp;
+}
+
+char *strcat(char *dst, const char *src)
+{
+  assert(dst != NULL);
+  char *tmp = dst + strlen(dst);
+  while (*src)
+    *tmp++ = *src++;
+  *tmp = '\0';
   return dst;
 }
 
-char *strncpy(char *dst, const char *src, size_t n) {
-  char *d = dst;
-
-  while (n) {
-    if ((*d = *src) != 0) src++;
-    ++d;
-    --n;
+int strcmp(const char *s1, const char *s2)
+{
+  while (*s1)
+  {
+    if (*s1 != *s2)
+      break;
+    s1++;
+    s2++;
   }
-
-  return dst;
+  return *(const unsigned char *)s1 - *(const unsigned char *)s2;
 }
 
-char *strcat(char *dst, const char *src) {
-    char *d = dst;
-
-    while (*d++);
-    --d;
-    while ((*d++ = *src++) != 0);
-
-    return dst;
-}
-
-int strcmp(const char *s1, const char *s2) {
-  while (*((char *)s1) == *((char *)s2)) {
-    if (!*s1++) {
-        return 0;
-    }
-    ++s2;
+int strncmp(const char *s1, const char *s2, size_t n)
+{
+  while (*s1 && --n)
+  {
+    if (*s1 != *s2)
+      break;
+    s1++;
+    s2++;
   }
-
-  return (*((char *)s1) < *((char *)s2)) ? -1 : 1;
+  return *(const unsigned char *)s1 - *(const unsigned char *)s2;
 }
 
-int strncmp(const char *s1, const char *s2, size_t n) {
-  while (n && (*((char *)s1) == *((char *)s2))) {
-		if (!*s1++) {
-			return 0;
-		}
-		++s2;
-		--n;
-	}
-
-	return (n == 0) ? 0 : (*((char *)s1) - *((char *)s2));
-}
-
-void *memset(void *s, int c, size_t n) {
-  char *tmp = (char *)s;
-
-  for (size_t i = 0; i < n; i++) {
-    tmp[i] = c;
-  }
-
+void *memset(void *s, int c, size_t n)
+{
+  char *tmp = s;
+  while (n--)
+    *tmp++ = c;
   return s;
 }
 
-
-/**
- * 此函数处理内存重叠情况
-*/
-void *memmove(void *dst, const void *src, size_t n) {
-    char *d = (char *)dst;
-    const char *s = (const char *)src;
-
-    if (s >= d) {
-        while (n) {
-            *d++ = *s++;
-            --n;
-        }
-    } else {
-        while (n) {
-            --n;
-            d[n] = s[n];
-        }
-    }
-
-    return dst;
+void *memmove(void *dst, const void *src, size_t n)
+{
+  assert(dst != NULL);
+  for (int i = n - 1; i >= 0; i--)
+    *((char *)dst + i) = *((char *)src + i);
+  return dst;
 }
 
-/**
- * 此函数不处理内存重叠，涉及内存重叠使用memmove
-*/
-void *memcpy(void *out, const void *in, size_t n) {
-  char *o = (char *)out;
-  const char *i = (const char *)in;
-
-  while (n--) {
-    *o++ = *i++;
-  }
-
+void *memcpy(void *out, const void *in, size_t n)
+{
+  // assert(out != NULL);
+  for (int i = 0; i < n; i++)
+    *((char *)out + i) = *((char *)in + i);
   return out;
 }
 
-/**
- * 返回-1表示s1 < s2, 返回1表示s1 > s2, 返回0表示相等
-*/
-int memcmp(const void *s1, const void *s2, size_t n) {
-    const char *p1 = s1;
-    const char *p2 = s2;
-
-    for (size_t i = 0; i < n; i++) {
-        if (p1[i] < p2[i]) {
-            return -1;
-        } else if (p1[i] > p2[i]) {
-            return 1;
-        }
-    }
-    return 0;
+int memcmp(const void *s1, const void *s2, size_t n)
+{
+  char *tmp1 = (char *)s1;
+  char *tmp2 = (char *)s2;
+  while (*tmp1 && --n)
+  {
+    if (*tmp1 != *tmp2)
+      break;
+    tmp1++;
+    tmp2++;
+  }
+  return *tmp1 - *tmp2;
 }
 
 #endif
