ddd61db669c2042b9e1325db3ce21e15b40cbf9e
tracer-ysyx <tracer@ysyx.org>
2025-09-03 01:28:21 -0700

    > sim RTL ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:28:21 up 18:03,  1 user,  load average: 0.44, 0.42, 0.56

--- npc/csrc/mydpic.cpp
+++ npc/csrc/mydpic.cpp
@@ -43,7 +43,6 @@ extern "C" void dcache_hit_count() {
 
 
 extern "C" void set_nextpc(int nextpc, int inst,svBit commit_valid) {
-    static bool isfirst_inst = true;
     // NOP 指令对于的 PC 为 0
     if (nextpc == 0 || commit_valid == 0) {
         return;
@@ -64,11 +63,6 @@ extern "C" void set_nextpc(int nextpc, int inst,svBit commit_valid) {
 #ifdef MTRACH
     printf("set_nextpc:%x\n", (void*)nextpc);
 #endif
-    if (isfirst_inst) {
-        printf("isfirst_inst\n");
-        isfirst_inst = false;
-        return;
-    }
 
     mysim_p->commited_list.nextpc.push_back(nextpc);
 }
