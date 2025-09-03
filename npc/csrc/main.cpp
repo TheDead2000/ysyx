572b5e28caf4ebe4e1e911cf3b3506a6ddfd1ac6
tracer-ysyx <tracer@ysyx.org>
2025-09-03 01:04:26 -0700

    > sim RTL ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:04:26 up 17:39,  1 user,  load average: 0.88, 1.13, 1.24

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -69,9 +69,9 @@ int main(int argc, char* argv[]) {
   int retCode;
 
 #ifdef TOP_TRACE
-  mysim_p->u_difftest.init(nemu_so_path, file_size, 0);
-  c.executeCommand("sdb on all");
-  c.executeCommand("sdb off reg");
+  // mysim_p->u_difftest.init(nemu_so_path, file_size, 0);
+  // c.executeCommand("sdb on all");
+  // c.executeCommand("sdb off reg");
 #endif
 
 #ifdef AUTO_RUN
