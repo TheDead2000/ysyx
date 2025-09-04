71049532e1b5e72c57a5ec0b4f9ad6f9889cfce0
tracer-ysyx <tracer@ysyx.org>
2025-09-03 23:07:54 -0700

    > sim RTL ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:07:54 up  9:14,  1 user,  load average: 2.49, 1.63, 1.45

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -22,7 +22,7 @@ void hello_fun(void *arg) {
 void init_proc() {
   switch_boot_pcb();
 
-  // Log("Initializing processes...");
+  Log("Initializing processes...");
 
   // load program here
   naive_uload(NULL, "/bin/dummy");
