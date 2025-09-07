6f91df699dc8f4b7a60db0140dd7bff7fd84c47d
tracer-ysyx <tracer@ysyx.org>
2025-09-07 04:55:56 -0700

    > compile NEMU ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  04:55:56 up  9:28,  1 user,  load average: 1.30, 1.26, 0.94

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -32,8 +32,10 @@ void init_proc() {
   // context_kload(&pcb[1], hello_fun, "BBB");
   char *argv[] = {NULL};
   char *envp[] = {NULL};
-  context_uload(&pcb[0], "/bin/menu", argv, envp);
-  context_uload(&pcb[2], "/bin/hello", argv, envp);
+  // context_uload(&pcb[0], "/bin/menu", argv, envp);
+  // context_uload(&pcb[2], "/bin/hello", argv, envp);
+  context_kload(&pcb[0], hello_fun, "AAA");
+  context_uload(&pcb[1], "/bin/exec-test", argv, envp);
 
   // yield();  
   // assert(0);
