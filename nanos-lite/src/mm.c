c63b69cc57a3f5c1c015d8e18f6261ec1ef6a26b
tracer-ysyx <tracer@ysyx.org>
2025-09-08 02:13:55 -0700

    > compile NEMU ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  02:13:55 up 20:10,  1 user,  load average: 2.54, 1.40, 0.90

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -1,17 +1,21 @@
-#include "proc.h"
+#include "am.h"
 #include <memory.h>
-#include <stdint.h>
-#include <stdlib.h>
+#include <proc.h>
 
 static void *pf = NULL;
 
 void* new_page(size_t nr_page) {
-  return NULL;
+    void *old_pf = pf;
+    pf = (void *)((char *)pf + nr_page * PGSIZE);
+    return old_pf;          // PA 手册要求返回地址段的首地址，所以返回 old_pf
 }
 
 #ifdef HAS_VME
 static void* pg_alloc(int n) {
-  return NULL;
+  assert(n % (4096) == 0);
+  void *ret = new_page(n / (4096));
+  memset(ret, 0, n);
+  return ret;
 }
 #endif
 
@@ -19,9 +23,16 @@ void free_page(void *p) {
   panic("not implement yet");
 }
 
-/* The brk() system call handler. */
+/* The brk() system call handler. brk is end address of allocated space 
+ * In map, pcb->max_brk is aligned to 4KB*/
+extern PCB *current;
 int mm_brk(uintptr_t brk) {
-  return 0;
+    if (brk <= current->max_brk)
+        return 0;
+    for (; current->max_brk < brk; current->max_brk += PGSIZE) {
+        map(&current->as, (void *)current->max_brk, pg_alloc(PGSIZE), PTE_V | PTE_R | PTE_W | PTE_X);
+    }
+    return 0;
 }
 
 void init_mm() {
@@ -29,6 +40,7 @@ void init_mm() {
   Log("free physical pages starting from %p", pf);
 
 #ifdef HAS_VME
+  Log("vme init");
   vme_init(pg_alloc, free_page);
 #endif
 }
