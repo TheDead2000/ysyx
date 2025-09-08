d74f4cf1869df8e9149efd4436ab93210119ca69
tracer-ysyx <tracer@ysyx.org>
2025-09-07 20:54:07 -0700

    > compile NEMU ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:54:07 up 14:50,  1 user,  load average: 0.36, 0.42, 0.57

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -30,7 +30,7 @@ void init_proc() {
   // context_kload(&pcb[0], (void *)hello_fun, "A");
   // context_kload(&pcb[1], (void *)hello_fun, "B");
 
-  switch_boot_pcb();
+  // switch_boot_pcb();
 
   Log("Initializing processes...");
 
