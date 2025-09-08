661f93cd15e8a5d0f664904d01d8bd863923fdf2
tracer-ysyx <tracer@ysyx.org>
2025-09-08 00:11:14 -0700

    > compile NEMU ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:11:14 up 18:07,  1 user,  load average: 1.91, 0.58, 0.19

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -3,7 +3,10 @@
 #include <klib-macros.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
+// #if defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
+
+void *addr=NULL;
 
 int rand(void) {
   // RAND_MAX assumed to be 32767
@@ -29,32 +32,24 @@ int atoi(const char* nptr) {
   return x;
 }
 
-char* head_start_p = NULL;
 void *malloc(size_t size) {
+  // printf("call malloc!");
+  if(!addr)
+    addr=heap.start;
+  void* ret=addr;
+  addr+=size;
+
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  if (head_start_p == NULL) {
-    head_start_p = heap.start;
-  }
-  // static int i = 0;
-  // if (i == 0) {
-  //   i++;
-  //   printf("malloc init\n");
-  //   init_memory_pool(heap.end - heap.start + 1, heap.start);
-  // }
-  // printf("malloc\n");
-  // return tlsf_malloc(size);
-  char* last_p = head_start_p;
-  head_start_p += size;
-  return last_p;
+
+  // panic("Not implemented");
 #endif
-  return NULL;
+  return ret;
 }
 
 void free(void *ptr) {
-  printf("free!\n");
 }
 
 #endif
