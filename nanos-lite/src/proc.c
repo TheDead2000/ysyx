5f54aaaae5133af5830d7ae68c4a3cb215447096
tracer-ysyx <tracer@ysyx.org>
2025-09-08 01:49:44 -0700

    > compile NEMU ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:49:44 up 19:45,  1 user,  load average: 0.42, 0.70, 0.69

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -27,9 +27,9 @@ void init_proc() {
   char *envp[] = {NULL};
   // context_uload(&pcb[0], "/bin/dummy", argv,envp);
   // context_uload(&pcb[1], "/bin/menu", argv, envp);
-  // context_kload(&pcb[0], hello_fun, "Kthread");
+  context_kload(&pcb[0], hello_fun, "Kthread");
   // context_kload(&pcb[1], hello_fun, "Kthread2");
-  context_uload(&pcb[0], "/bin/hello", argv, envp);
+  // context_uload(&pcb[0], "/bin/hello", argv, envp);
   context_uload(&pcb[1], "/bin/menu", argv, envp);
   switch_boot_pcb();
 
