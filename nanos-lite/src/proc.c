886e1c841c336c4bfbbfb40509ed151419de7254
tracer-ysyx <tracer@ysyx.org>
2025-09-08 22:36:09 -0700

    > compile NEMU ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:36:09 up  2:25,  1 user,  load average: 1.99, 1.92, 1.58

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -13,7 +13,7 @@ void switch_boot_pcb() {
 void hello_fun(void *arg) {
   int j = 1;
   while (1) {
-      if (j % 200 == 0) {
+      if (j % 2000 == 0) {
         Log("Hello World from Nanos-lite with arg '%s' for the %dth time!", (char *)arg, j);
       }
     j ++;
