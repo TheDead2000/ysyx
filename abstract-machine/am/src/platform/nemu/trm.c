3c20d98f5a8e52194c68833d545dcbdbca68faf8
tracer-ysyx <tracer@ysyx.org>
2025-09-08 22:57:30 -0700

    > compile NEMU ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:57:30 up  2:46,  1 user,  load average: 2.06, 1.35, 1.44

--- abstract-machine/am/src/platform/nemu/trm.c
+++ abstract-machine/am/src/platform/nemu/trm.c
@@ -1,5 +1,6 @@
 #include <am.h>
 #include <nemu.h>
+#include <stdio.h>
 
 extern char _heap_start;
 int main(const char *args);
@@ -9,7 +10,6 @@ Area heap = RANGE(&_heap_start, PMEM_END);
 #define MAINARGS ""
 #endif
 static const char mainargs[] = MAINARGS;
-
 void putch(char ch) {
   outb(SERIAL_PORT, ch);
 }
