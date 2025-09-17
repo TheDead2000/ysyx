66511d21568a4576fb3fd83b066bc933e143019a
tracer-ysyx <tracer@ysyx.org>
2025-09-17 00:22:21 -0700

    > sim RTL ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:22:21 up 1 day,  3:29,  1 user,  load average: 2.08, 0.96, 0.95

--- npc/vsrc/usr/pipeline/pipline_control.v
+++ npc/vsrc/usr/pipeline/pipline_control.v
@@ -59,8 +59,8 @@ module pipline_control (
       _flush = ram_mem_flush;
     end 
     else if( ram_stall_req_if) begin
-        _stall = ram_if_stall;
-        _flush = ram_if_flush;
+        _stall = ram_mem_stall;
+        _flush = ram_mem_flush;
       end
 
       // 中断|异常,(发生在 mem 阶段)
