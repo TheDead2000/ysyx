b0d6b70a5338e1a20c507cc350e9fc570f0ac5c1
tracer-ysyx <tracer@ysyx.org>
2025-09-21 22:18:41 -0700

    > sim RTL ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:18:41 up 30 min,  1 user,  load average: 3.12, 1.12, 0.73

---
+++ abstract-machine/scripts/platform/ysyxsoc.mk
@@ -0,0 +1,35 @@
+AM_SRCS := riscv/ysyxsoc/start.S \
+           riscv/ysyxsoc/trm.c \
+		    platform/dummy/vme.c \
+           platform/dummy/mpe.c
+#            riscv/ysyxsoc/ioe.c \
+#            riscv/ysyxsoc/timer.c \
+#            riscv/ysyxsoc/input.c \
+#            riscv/ysyxsoc/cte.c \
+#            riscv/ysyxsoc/trap.S \
+#            riscv/ysyxsoc/serial.c \
+#            riscv/ysyxsoc/gpu.c \
+
+
+CFLAGS    += -fdata-sections -ffunction-sections
+
+LDFLAGS   += -T $(AM_HOME)/scripts/linker_ysyxsoc.ld
+LDFLAGS   += --defsym=_stack_size=1K
+LDFLAGS   += --gc-sections -e _start
+# LDFLAGS   += --print-map
+# LDFLAGS   += -Map out.map
+CFLAGS += -DMAINARGS=\"$(mainargs)\"
+CFLAGS += -I$(AM_HOME)/am/src/riscv/ysyxsoc/include
+ifneq ($(DIFFTEST),)
+CFLAGS += -DDIFFTEST_ON
+endif
+.PHONY: $(AM_HOME)/am/src/riscv/ysyxsoc/trm.c
+
+image: $(IMAGE).elf
+	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
+	@echo + OBJCOPY "->" $(IMAGE_REL).bin
+	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
+
+run: image
+	$(MAKE) -C $(NPC_HOME) clean
+	$(MAKE) -C $(NPC_HOME) run IMAGE=$(IMAGE).bin
