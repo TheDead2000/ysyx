3f1619095bda7d5fce3c49c70e780d05100bb0da
tracer-ysyx <tracer@ysyx.org>
2025-09-24 04:19:48 -0700

    > sim RTL ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  04:19:48 up 1 day,  2:32,  1 user,  load average: 0.57, 0.54, 0.37

--- am-kernels/tests/cpu-tests/tests/dummy.c
+++ am-kernels/tests/cpu-tests/tests/dummy.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 #include <klib.h>
 int main() {
-  putch('h');
-  putch('e');
-  putch('l');
-  putch('l');
-  putch('o');
-  putch(',');
-  putch('w');
-  putch('o');
-  putch('r');
-  putch('l');
-  putch('d');
-  // printf("hello,world!\n");
+  // putch('h');
+  // putch('e');
+  // putch('l');
+  // putch('l');
+  // putch('o');
+  // putch(',');
+  // putch('w');
+  // putch('o');
+  // putch('r');
+  // putch('l');
+  // putch('d');
+  printf("hello,world!\n");
   return 0;
 }
