4e69814ecc282a2d46594c6623f6da1fc48746bd
tracer-ysyx <tracer@ysyx.org>
2025-09-23 19:54:17 -0700

    > sim RTL ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:54:17 up 18:06,  1 user,  load average: 0.19, 0.31, 0.18

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -108,7 +108,7 @@ void spi_tx_start() {
 }
 
 void putch(char ch) {
-  while((inb(UART_REG_LS) & 0x20) == 0);
+  // while((inb(UART_REG_LS) & 0x20) == 0);
   outb(UART_REG_TX, ch);
 }
 
@@ -121,9 +121,10 @@ void halt(int code) {
 
 void _trm_init() {
 #ifndef DIFFTEST_ON
-  init_uart(115200);
+  // init_uart(115200);
 //   brandShow();
 #endif
+  putch('a');
   int ret = main(mainargs);
   halt(ret);
 }
