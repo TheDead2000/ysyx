968d9ce7ea07d350bf1ad98177e3b2e0a05f5a58
tracer-ysyx <tracer@ysyx.org>
2025-09-27 19:51:00 -0700

    > compile NEMU ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:51:00 up 2 days, 18 min,  1 user,  load average: 1.30, 1.09, 3.31

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -12,7 +12,7 @@ BUILD_DIR = $(WORK_DIR)/build
 
 INC_PATH := $(WORK_DIR)/include $(INC_PATH)
 OBJ_DIR  = $(BUILD_DIR)/obj-$(NAME)$(SO)
-BINARY   = $(BUILD_DIR)/$(NAME)
+BINARY   = $(BUILD_DIR)/$(NAME)$(SO)
 
 # Compilation flags
 ifeq ($(CC),clang)
