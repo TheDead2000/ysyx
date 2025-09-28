2f714039bfb93ed6c04ba4331fb86dba3ca14123
tracer-ysyx <tracer@ysyx.org>
2025-09-27 22:40:49 -0700

    > sim RTL ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:40:49 up 2 days,  3:08,  1 user,  load average: 0.74, 0.59, 0.71

--- am-kernels/tests/cpu-tests/tests/dummy.c
+++ am-kernels/tests/cpu-tests/tests/dummy.c
@@ -2,20 +2,25 @@
 #include <stdio.h>
 #include <klib.h>
 // char* str = "%shello,wolrd\n";
+// int main() {
+//   // putch('h');
+//   // putch('e');
+//   // putch('l');
+//   // putch('l');
+//   // putch('o');
+//   // putch(',');
+//   // putch('w');
+//   // putch('o');
+//   // putch('r');
+//   // putch('l');
+//   // putch('d');
+//   // putch('\n');
+//   // printf("%s",str);
+//   printf("hello,world!\n");
+//   return 0;
+// }
 int main() {
-  // putch('h');
-  // putch('e');
-  // putch('l');
-  // putch('l');
-  // putch('o');
-  // putch(',');
-  // putch('w');
-  // putch('o');
-  // putch('r');
-  // putch('l');
-  // putch('d');
-  // putch('\n');
-  // printf("%s",str);
-  printf("hello,world!\n");
-  return 0;
+char str[] = "hello,world!\n";
+printf(str);
+return 0;
 }
