9b84243c838b113c472a17f3de39aa80591b3c5a
tracer-ysyx <tracer@ysyx.org>
2025-09-28 04:11:37 -0700

    > sim RTL ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  04:11:37 up 2 days,  8:39,  1 user,  load average: 0.68, 0.57, 0.48

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -47,7 +47,7 @@ int printf_call_count = 0;
   // }
   void test_flash_byte_access() {
     // 测试直接访问 Flash 中的不同字节
-    const char *flash_str = (const char*)0x300006b4; // 你的字符串地址
+    const char *flash_str = (const char*)0xa0000530; // 你的字符串地址
     
     putch('F');
     putch(':');
@@ -64,10 +64,10 @@ int printf(const char *fmt, ...) {
   char out[BUFFER_LENGH];
   va_list args;
   va_start(args, fmt);
-  for(int i = 0 ; i < 10 ;i++  )
-  {
-    putch(fmt[i]);
-  }
+  // for(int i = 0 ; i < 10 ;i++  )
+  // {
+  //   putch(fmt[i]);
+  // }
   //   const uint32_t *word_ptr = (const uint32_t*)fmt;
   
   // for(int i = 0; i < 10; i++) {
@@ -86,7 +86,7 @@ int printf(const char *fmt, ...) {
   //   putch(c);
   //   putch('|');
   // }
-  // test_flash_byte_access();
+  test_flash_byte_access();
   int len = vsprintf(out, fmt, args);
   va_end(args);
   putstr(out);
