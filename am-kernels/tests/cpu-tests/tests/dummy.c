b9c160c69a084a13a36e1afefca8739f64607371
tracer-ysyx <tracer@ysyx.org>
2025-09-27 18:53:56 -0700

    > sim RTL ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:53:56 up 1 day, 23:21,  1 user,  load average: 4.51, 5.15, 2.42

--- am-kernels/tests/cpu-tests/tests/dummy.c
+++ am-kernels/tests/cpu-tests/tests/dummy.c
@@ -3,18 +3,19 @@
 #include <klib.h>
 char* str = "hello,wolrd\n";
 int main() {
-  putch('h');
-  putch('e');
-  putch('l');
-  putch('l');
-  putch('o');
-  putch(',');
-  putch('w');
-  putch('o');
-  putch('r');
-  putch('l');
-  putch('d');
-  putch('\n');
+  // putch('h');
+  // putch('e');
+  // putch('l');
+  // putch('l');
+  // putch('o');
+  // putch(',');
+  // putch('w');
+  // putch('o');
+  // putch('r');
+  // putch('l');
+  // putch('d');
+  // putch('\n');
   // printf("%s",str);
+  printf("hello,world!\n");
   return 0;
 }
