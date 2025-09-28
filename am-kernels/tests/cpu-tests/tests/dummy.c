b682f5fb57bb1a12bfd87f219edf589ecf33b37d
tracer-ysyx <tracer@ysyx.org>
2025-09-28 06:34:06 -0700

    > sim RTL ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  06:34:06 up 2 days, 11:01,  1 user,  load average: 1.00, 0.84, 0.82

--- am-kernels/tests/cpu-tests/tests/dummy.c
+++ am-kernels/tests/cpu-tests/tests/dummy.c
@@ -19,20 +19,46 @@
 //   printf("hello,world!\n");
 //   return 0;
 // }
-int main() {
-  // 直接测试内存访问
-  const char *str = "hello,world!\n";
+// int main() {
+//   // 直接测试内存访问
+//   const char *str = "hello,world!\n";
   
-  // putch(str[0]);
-  // putch('|');
-  // putch(str[1]); 
-  // putch('|');
-  // putch(str[2]);
-  // putch('|');
-  // putch(str[3]);
-  // putch('|');
-  // putch(str[4]);
-  // putch('\n');
-  printf(str);
-  return 0;
+//   // putch(str[0]);
+//   // putch('|');
+//   // putch(str[1]); 
+//   // putch('|');
+//   // putch(str[2]);
+//   // putch('|');
+//   // putch(str[3]);
+//   // putch('|');
+//   // putch(str[4]);
+//   // putch('\n');
+//   printf(str);
+//   return 0;
+// }
+
+void test_memory_layout() {
+    // 测试字符串地址
+    const char *test_str = "hello,world!\n";
+    
+    putch('@');
+    // 打印字符串地址
+    uint32_t addr = (uint32_t)test_str;
+    for(int i = 7; i >= 0; i--) {
+        putch("0123456789ABCDEF"[(addr >> (i*4)) & 0xF]);
+    }
+    putch(':');
+    
+    // 测试访问
+    for(int i = 0; i < 5; i++) {
+        putch(test_str[i]);
+        putch('|');
+    }
+    putch('\n');
+}
+
+int main() {
+    test_memory_layout();
+    printf("hello,world!\n");
+    return 0;
 }
