f90f7b729693c7eff234a610f559377bc246b9f4
tracer-ysyx <tracer@ysyx.org>
2025-10-10 20:21:19 -0700

    > sim RTL ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:21:19 up 3 days, 17 min,  1 user,  load average: 1.92, 2.02, 3.89

--- nvboard/src/uart.cpp
+++ nvboard/src/uart.cpp
@@ -52,7 +52,7 @@ void UART::tx_receive() {
     if (tx) { // stop bit
       tx_state = 0;
       term->feed_ch(tx_data);
-      printf("receive:%x\n",tx_data);
+      // printf("receive:%x\n",tx_data);
       need_update_gui = true;
     }
   }
