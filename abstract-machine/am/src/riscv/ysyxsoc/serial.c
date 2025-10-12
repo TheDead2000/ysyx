94a3b9904c9632cdde5657ae6d78f47d066898b0
tracer-ysyx <tracer@ysyx.org>
2025-10-11 19:16:59 -0700

    > sim RTL ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:16:59 up 3 days, 12:19,  1 user,  load average: 3.66, 2.91, 2.57

--- abstract-machine/am/src/riscv/ysyxsoc/serial.c
+++ abstract-machine/am/src/riscv/ysyxsoc/serial.c
@@ -2,6 +2,7 @@
 #include "ysyxsoc.h"
 #include "klib.h"
 void __am_uart_rx(AM_UART_RX_T *cfg) {
+    printf("__am_uart_rx\n");
     if((inb(UART_REG_LS) & 0x1) == 0x1){
         printf("revecive some\n");
         cfg->data = inb(UART_REG_RX);
