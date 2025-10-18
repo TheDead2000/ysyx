1c6aec58a029bc57b007ca1d977871aa452e1fde
tracer-ysyx <tracer@ysyx.org>
2025-10-18 06:26:40 -0700

    > sim RTL ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  06:26:40 up  1:39,  1 user,  load average: 3.57, 1.48, 0.83

--- am-kernels/kernels/litenes/src/fce.c
+++ am-kernels/kernels/litenes/src/fce.c
@@ -63,6 +63,7 @@ int fce_load_rom(char *rom) {
 
   // Copying CHR pages into MMC and PPU
   int i;
+  printf("ready to copy\n");
   for (i = 0; i < fce_rom_header->chr_block_count; i++) {
     byte *blk = romread(0x2000);
     mmc_append_chr_rom_page(blk);
@@ -71,14 +72,19 @@ int fce_load_rom(char *rom) {
       ppu_copy(0x0000, blk, 0x2000);
     }
   }
+  printf("copy done\n");
 
   return 0;
 }
 
 void fce_init() {
+  printf("Initializing FCE...\n");
   cpu_init();
+  printf("CPU initialized.\n");
   ppu_init();
+  printf("PPU initialized.\n");
   ppu_set_mirroring(fce_rom_header->rom_type & 1);
+  printf("Mirroring set.\n");
   cpu_reset();
 }
 
@@ -113,6 +119,7 @@ void fce_run() {
 
     nr_draw ++;
     int upt = uptime_ms();
+    printf("upt=%d,gtime=%d\n",upt,gtime);
     if (upt - last > 1000) {
       last = upt;
       for (int i = 0; i < 80; i++) putch('\b');
