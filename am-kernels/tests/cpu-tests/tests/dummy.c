dfa5cecd0d0102d71ca036553d3e5d706ff1d42a
tracer-ysyx <tracer@ysyx.org>
2025-10-21 06:48:16 -0700

    > sim RTL ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  06:48:16 up 1 day,  3:28,  1 user,  load average: 1.62, 0.98, 1.06

--- am-kernels/tests/cpu-tests/tests/dummy.c
+++ am-kernels/tests/cpu-tests/tests/dummy.c
@@ -2,6 +2,33 @@
 #include <stdio.h>
 #include <klib.h>
 
+void test_amoswap() {
+    printf("Testing amoswap.w instruction (fixed version)...\n");
+    
+    // 初始化内存位置和寄存器值
+    uint32_t memory_value = 0x12345678;
+    uint32_t new_value = 0xABCDEF00;
+    uint32_t old_value;
+    
+    printf("Initial memory value: 0x%08X\n", memory_value);
+    printf("New value to swap: 0x%08X\n", new_value);
+    
+    // 使用正确的地址加载方式
+    __asm__ volatile (
+        "mv x16, %[addr]\n"           // 将内存地址移动到x16
+        "mv x17, %[new_val]\n"        // 将新值移动到x17
+        "amoswap.w %[old_val], x17, (x16)\n"  // 执行原子交换，结果保存到old_val
+        : [old_val] "=r" (old_value)
+        : [addr] "r" (&memory_value), [new_val] "r" (new_value)
+        : "x16", "x17", "memory"
+    );
+    
+    printf("After amoswap.w:\n");
+    printf("Memory now contains: 0x%08X\n", memory_value);
+    printf("Returned old value: 0x%08X\n", old_value);
+}
+
+
 int main() {
   // const char *str = "hello,world!\n";
   // putch('h');
@@ -18,10 +45,12 @@ int main() {
   // putch('\n');
   // printf("%s",str);
   // printf("%s",str);
-  static const char *p = "help\ndate\nversion\nfree\nps\npwd\nls\nmemtrace\nmemcheck\nutest_list\n";
-  while((*p) != '\0')
-  printf("get ch %c \n",(*p++));
-  while(1);
+  // static const char *p = "help\ndate\nversion\nfree\nps\npwd\nls\nmemtrace\nmemcheck\nutest_list\n";
+  // while((*p) != '\0')
+  // printf("get ch %c \n",(*p++));
+  // while(1);
+  test_amoswap();
+
   return 0;
 }
 // int main() {
