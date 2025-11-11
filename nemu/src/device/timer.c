7ce4565dd4928efe1e2ce746b99a51a6b6b3d132
tracer-ysyx <tracer@ysyx.org>
2025-11-11 04:17:40 -0800

    > compile NEMU ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  04:17:40 up 18:52,  1 user,  load average: 1.69, 1.02, 0.77

--- nemu/src/device/timer.c
+++ nemu/src/device/timer.c
@@ -17,6 +17,7 @@
 #include <device/alarm.h>
 #include <utils.h>
 
+#define CONFIG_RTC_MMIO 0xa0000048
 static uint32_t *rtc_port_base = NULL;
 
 static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
