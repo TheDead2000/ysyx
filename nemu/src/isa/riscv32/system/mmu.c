4de333748d6b0659a2b15961fa6ef220fcf7bb8d
tracer-ysyx <tracer@ysyx.org>
2025-11-12 20:21:01 -0800

    > compile NEMU ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:21:01 up 20 min,  1 user,  load average: 0.73, 0.88, 0.56

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -60,7 +60,7 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
     printf("pte_1_addr=%x, pte_1=%x\n", pte_1_addr, pte_1);
     printf("pte_1 & PTE_V=%x\n", pte_1 & PTE_V);
     printf("vaddr=%x\n", vaddr);
-    Assert(pte_1 & PTE_V, "first class pte is not valid, vaddr=%x", vaddr);
+    //Assert(pte_1 & PTE_V, "first class pte is not valid, vaddr=%x", vaddr);
 
     // 检查一级页表项是否是叶子页表项（超级页）
     if ((pte_1 & PTE_R) || (pte_1 & PTE_W) || (pte_1 & PTE_X)) {
