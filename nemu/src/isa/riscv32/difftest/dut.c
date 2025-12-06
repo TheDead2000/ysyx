0213537d7d0db792128371db23a817a214ae42e0
tracer-ysyx <tracer@ysyx.org>
2025-12-06 06:54:17 -0800

    > compile NEMU ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  06:54:17 up 19:21,  1 user,  load average: 0.93, 0.75, 0.69

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -16,14 +16,25 @@
 #include <isa.h>
 #include <cpu/difftest.h>
 #include "../local-include/reg.h"
-
+extern const char* regs[];
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  if (ref_r->pc != cpu.pc) return false;
-  for (int i = 0; i < 32; i++) {
-    if (ref_r->gpr[i] != cpu.gpr[i])
-      return false;
+  bool ret=true;
+  if(!difftest_check_reg("pc",pc,ref_r->pc,cpu.pc))
+    ret = false;
+  for(int i=0;i<32;i++){
+    if(!difftest_check_reg(regs[i],pc,ref_r->gpr[i],cpu.gpr[i]))
+      ret =  false;
   }
-  return true;
+  return ret;
+}
+
+bool isa_difftest_checkcsrs(word_t *ref_csr,vaddr_t pc) {
+  bool ret=true;
+  for(int i=0;difftest_csr_idx[i]!=0;i++){
+    if(!difftest_check_reg(difftest_csr_name[i],pc,ref_csr[i],cpu.csr[difftest_csr_idx[i]]))
+      ret =  false;
+  }
+  return ret;
 }
 
 void isa_difftest_attach() {
