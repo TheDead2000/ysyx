9379b7cb18020f825eb4e4dd8f4ba2ef9fdb965c
tracer-ysyx <tracer@ysyx.org>
2025-12-07 04:39:10 -0800

    > compile NEMU ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  04:39:10 up 1 day,  4:00,  1 user,  load average: 0.98, 0.89, 0.56

--- nemu/tools/gdbstub.mk
+++ nemu/tools/gdbstub.mk
@@ -1,13 +1,13 @@
-# included in native.mk
+# # included in native.mk
 
-LIB_GDBSTUB := $(NEMU_HOME)/tools/mini-gdbstub/build/libgdbstub.a
+# LIB_GDBSTUB := $(NEMU_HOME)/tools/mini-gdbstub/build/libgdbstub.a
 
 
-ifndef CONFIG_DIFFTEST_REF_NEMU
-	LDFLAGS += -L$(NEMU_HOME)/tools/mini-gdbstub/build/ 
-	CFLAGS += -I$(NEMU_HOME)/tools/mini-gdbstub/include
-$(LIB_GDBSTUB):
-	cd $(NEMU_HOME)/tools/mini-gdbstub && make
-endif
+# ifndef CONFIG_DIFFTEST_REF_NEMU
+# 	LDFLAGS += -L$(NEMU_HOME)/tools/mini-gdbstub/build/ -lgdbstub
+# 	CFLAGS += -I$(NEMU_HOME)/tools/mini-gdbstub/include
+# $(LIB_GDBSTUB):
+# 	cd $(NEMU_HOME)/tools/mini-gdbstub && make
+# endif
 
-.PHONY: $(LIB_GDBSTUB)
+# .PHONY: $(LIB_GDBSTUB)
