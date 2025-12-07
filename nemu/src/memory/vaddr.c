cd307bf521f44f16c3f9d3391f569dcacac06bf8
tracer-ysyx <tracer@ysyx.org>
2025-12-07 02:04:39 -0800

    > compile NEMU ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  02:04:39 up 1 day,  1:25,  1 user,  load average: 2.08, 1.51, 1.39

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -16,24 +16,112 @@
 #include <isa.h>
 #include <memory/paddr.h>
 
+/***************************************************************************************
+* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+*
+* NEMU is licensed under Mulan PSL v2.
+* You can use this software according to the terms and conditions of the Mulan PSL v2.
+* You may obtain a copy of Mulan PSL v2 at:
+*          http://license.coscl.org.cn/MulanPSL2
+*
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+*
+* See the Mulan PSL v2 for more details.
+***************************************************************************************/
+
+#include "common.h"
+#include <isa.h>
+#include <memory/paddr.h>
+#include <setjmp.h>
+
+uint32_t mem_access_status=NEMU_MEMA_NORMAL;
+extern jmp_buf memerr_jump_buffer;
+
+// TODO-MEM_RET_FAIL/CROSSPAGE不应该直接assert,应该设置状态!
+
 word_t vaddr_ifetch(vaddr_t addr, int len) {
-      //printf("ifetch addr=%x, len=%d\n", addr, len);
-  if (isa_mmu_check(addr, len, MEM_TYPE_IFETCH))
-    addr = isa_mmu_translate(addr, len, MEM_TYPE_IFETCH);
-  return paddr_read(addr, len);
+  paddr_t paddr=addr;
+  switch (isa_mmu_check(paddr, len, 0)) {
+  case MMU_DIRECT:
+    break;
+  case MMU_TRANSLATE:
+    paddr = isa_mmu_translate(addr,len,NEMU_MEM_EXEC);
+    break;
+  case MMU_FAIL:
+    assert(0);
+    break;
+  }
+  // if(paddr==MEM_RET_FAIL){
+  //   longjmp(memerr_jump_buffer, NEMU_MEMA_FETCHERR);
+  // }
+ // assert(paddr != MEM_RET_CROSS_PAGE);
+  //assert(paddr != MEM_RET_FAIL);
+  return paddr_read(paddr, len);
 }
 
 word_t vaddr_read(vaddr_t addr, int len) {
-  //printf("vaddr_read: addr=%x, len=%d\n", addr, len);
-  if (isa_mmu_check(addr, len, MEM_TYPE_READ))
-    addr = isa_mmu_translate(addr, len, MEM_TYPE_READ);
-
-  return paddr_read(addr, len);
+  // return paddr_read(addr, len);
+  paddr_t paddr=addr;
+  switch (isa_mmu_check(paddr, len, 0)) {
+  case MMU_DIRECT:
+    break;
+  case MMU_TRANSLATE:
+    paddr = isa_mmu_translate(addr,len,NEMU_MEM_READ);
+    break;
+  case MMU_FAIL:
+    assert(0);
+    break;
+  }
+  // if(paddr==MEM_RET_FAIL){
+  //   longjmp(memerr_jump_buffer, NEMU_MEMA_READERR);
+  // }
+  // assert(paddr != MEM_RET_CROSS_PAGE );
+  //assert(paddr != MEM_RET_FAIL);
+  return paddr_read(paddr, len);
 }
 
 void vaddr_write(vaddr_t addr, int len, word_t data) {
-  // printf("vaddr_write: addr=%x, len=%d, data=%x\n", addr, len, data);
-  if (isa_mmu_check(addr, len, MEM_TYPE_WRITE))
-    addr = isa_mmu_translate(addr, len, MEM_TYPE_WRITE);
-  paddr_write(addr, len, data);
+  // paddr_write(addr, len, data);
+  paddr_t paddr=addr;
+  switch (isa_mmu_check(paddr, len, 0)) {
+  case MMU_DIRECT:
+    break;
+  case MMU_TRANSLATE:
+    paddr = isa_mmu_translate(addr,len,NEMU_MEM_WRITE);
+    break;
+  case MMU_FAIL:
+    assert(0);
+    break;
+  }
+  // assert(paddr != MEM_RET_CROSS_PAGE);
+  // //assert(paddr != MEM_RET_FAIL);
+  // if(paddr==MEM_RET_FAIL){
+  //   longjmp(memerr_jump_buffer, NEMU_MEMA_STOREERR);
+  // }
+  paddr_write(paddr, len,data);
 }
+
+
+// word_t vaddr_ifetch(vaddr_t addr, int len) {
+//       //printf("ifetch addr=%x, len=%d\n", addr, len);
+//   if (isa_mmu_check(addr, len, MEM_TYPE_IFETCH))
+//     addr = isa_mmu_translate(addr, len, MEM_TYPE_IFETCH);
+//   return paddr_read(addr, len);
+// }
+
+// word_t vaddr_read(vaddr_t addr, int len) {
+//   //printf("vaddr_read: addr=%x, len=%d\n", addr, len);
+//   if (isa_mmu_check(addr, len, MEM_TYPE_READ))
+//     addr = isa_mmu_translate(addr, len, MEM_TYPE_READ);
+
+//   return paddr_read(addr, len);
+// }
+
+// void vaddr_write(vaddr_t addr, int len, word_t data) {
+//   // printf("vaddr_write: addr=%x, len=%d, data=%x\n", addr, len, data);
+//   if (isa_mmu_check(addr, len, MEM_TYPE_WRITE))
+//     addr = isa_mmu_translate(addr, len, MEM_TYPE_WRITE);
+//   paddr_write(addr, len, data);
+// }
