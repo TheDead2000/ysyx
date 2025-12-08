04ed59d420d45d4fdad67f7c3de5612b34cdf8b1
tracer-ysyx <tracer@ysyx.org>
2025-12-07 19:38:34 -0800

    > compile NEMU ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:38:34 up 1 day,  7:02,  1 user,  load average: 1.36, 0.99, 0.89

--- nemu/src/device/timer.c
+++ nemu/src/device/timer.c
@@ -18,33 +18,48 @@
 #include <device/map.h>
 #include <device/alarm.h>
 #include <utils.h>
+#include <isa.h>
+// 简单的定时器实现
+static uint32_t *clint_base = NULL;
+static uint64_t timer_ticks = 0;
 
-static uint32_t *rtc_port_base = NULL;
-
-static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
-  assert(offset == 0 || offset == 4);
-  if (!is_write && offset == 4) {
-    uint64_t us = get_time();
-    rtc_port_base[0] = (uint32_t)us;
-    rtc_port_base[1] = us >> 32;
-  }
+static void simple_clint_handler(uint32_t offset, int len, bool is_write) {
+    if (is_write) {
+        // 任何对 mtimecmp 的写入都清除中断
+        if (offset >= 8) { // mtimecmp 区域
+            cpu.csr[NEMU_CSR_MIP] &= ~(1 << 7);
+        }
+    } else {
+        // 读取 mtime 时返回递增的值
+        if (offset < 8) { // mtime 区域
+            timer_ticks++;
+            if (offset == 0) {
+                *(uint32_t*)clint_base = (uint32_t)(timer_ticks & 0xFFFFFFFF);
+            } else if (offset == 4) {
+                *(uint32_t*)clint_base = (uint32_t)(timer_ticks >> 32);
+            }
+        }
+    }
 }
 
-#ifndef CONFIG_TARGET_AM
-static void timer_intr() {
-  if (nemu_state.state == NEMU_RUNNING) {
-    extern void dev_raise_intr();
-    dev_raise_intr();
-  }
+// 定期触发中断
+static void simulate_timer_tick() {
+    // 定期设置中断
+    static int tick_count = 0;
+    tick_count++;
+    
+    // 每100次调用触发一次中断（模拟10ms）
+    if (tick_count % 100 == 0) {
+        cpu.csr[NEMU_CSR_MIP] |= (1 << 7);
+    }
+    
+    add_alarm_handle(100); // 每100微秒调用一次
 }
-#endif
 
 void init_timer() {
-  rtc_port_base = (uint32_t *)new_space(8);
-#ifdef CONFIG_HAS_PORT_IO
-  add_pio_map ("rtc", CONFIG_RTC_PORT, rtc_port_base, 8, rtc_io_handler);
-#else
-  add_mmio_map("rtc", CONFIG_RTC_MMIO, rtc_port_base, 8, rtc_io_handler);
-#endif
-  IFNDEF(CONFIG_TARGET_AM, add_alarm_handle(timer_intr));
+    clint_base = (uint32_t *)new_space(16);
+    add_mmio_map("clint", 0xa0000048, clint_base, 16, simple_clint_handler);
+    
+    // 启动模拟定时器
+    add_alarm_handle(100);
 }
