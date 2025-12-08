0a1a280fea651903b63d3d90b33d0f5f469272c7
tracer-ysyx <tracer@ysyx.org>
2025-12-08 06:06:57 -0800

    > compile NEMU ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  06:06:57 up 1 day, 17:30,  1 user,  load average: 1.04, 0.30, 0.10

--- nemu/src/device/timer.c
+++ nemu/src/device/timer.c
@@ -1,93 +1,50 @@
+/***************************************************************************************
+* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+*
+* NEMU is licensed under Mulan PSL v2.
+* You can use this software according to the terms and conditions of the Mulan PSL v2.
+* You may obtain a copy of Mulan PSL v2 at:
+*          http://license.coscl.org.cn/MulanPSL2
+*
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+*
+* See the Mulan PSL v2 for more details.
+***************************************************************************************/
+
+//模拟了i8253计时器的功能.
+
 #include <device/map.h>
 #include <device/alarm.h>
 #include <utils.h>
-#include <isa.h>
 
-static uint32_t *clint_base = NULL;
-static uint64_t mtime_val = 1000;
-static uint64_t mtimecmp_val = 0xFFFFFFFFFFFFFFFF;
-static int interrupt_counter = 0;
+static uint32_t *rtc_port_base = NULL;
 
-// 确保中断被启用
-static void ensure_interrupt_enabled() {
-    // 强制启用M模式中断
-    cpu.csr[NEMU_CSR_MSTATUS] |= (1 << 3); // MIE位
-    
-    // 同时确保S模式中断使能（如果当前在S模式）
-    cpu.csr[NEMU_CSR_SSTATUS] |= (1 << 1); // SIE位
-    
-    // 确保定时器中断使能
-    cpu.csr[NEMU_CSR_MIE] |= (1 << 7);    // MTIE
-    cpu.csr[NEMU_CSR_SIE] |= (1 << 5);    // STIE
+static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
+  assert(offset == 0 || offset == 4);
+  if (!is_write && offset == 4) {
+    uint64_t us = get_time();
+    rtc_port_base[0] = (uint32_t)us;
+    rtc_port_base[1] = us >> 32;
+  }
 }
 
-static void simple_clint_handler(uint32_t offset, int len, bool is_write) {
-    static uint64_t fast_time = 1000;
-    
-    Log("CLINT访问: offset=0x%x, is_write=%d", offset, is_write);
-    
-    if (is_write) {
-        if (offset >= 0x08) { // 写入mtimecmp
-            // 清除中断
-            cpu.csr[NEMU_CSR_MIP] &= ~(1 << 7);
-            
-            // 强制启用中断
-            ensure_interrupt_enabled();
-        }
-    } else {
-        if (offset < 0x08) { // 读取mtime
-            // 强制时间快速增加
-            fast_time += 1000000; // 1ms
-            
-            if (offset == 0x00) {
-                clint_base[0] = (uint32_t)(fast_time & 0xFFFFFFFF);
-            } else if (offset == 0x04) {
-                clint_base[1] = (uint32_t)(fast_time >> 32);
-            }
-            
-            // 强制触发中断
-            cpu.csr[NEMU_CSR_MIP] |= (1 << 7);
-            ensure_interrupt_enabled();
-            
-            Log("强制时间增加并触发中断: time=0x%lx", fast_time);
-        }
-    }
+#ifndef CONFIG_TARGET_AM
+static void timer_intr() {
+  if (nemu_state.state == NEMU_RUNNING) {
+    extern void dev_raise_intr();
+    dev_raise_intr();
+  }
 }
-
-static void timer_tick() {
-    static int tick_count = 0;
-    tick_count++;
-    
-    // 快速增加时间
-    mtime_val += 100000;
-    
-    // 确保中断被启用
-    ensure_interrupt_enabled();
-    
-    // 每2个tick触发一次中断
-    if (tick_count % 10 == 0) {
-        cpu.csr[NEMU_CSR_MIP] |= (1 << 7);
-        interrupt_counter++;
-        
-        Log("定时器中断触发 #%d: MSTATUS=0x%x, MIE=0x%x, MIP=0x%x", 
-            interrupt_counter, cpu.csr[NEMU_CSR_MSTATUS], 
-            cpu.csr[NEMU_CSR_MIE], cpu.csr[NEMU_CSR_MIP]);
-    }
-}
+#endif
 
 void init_timer() {
-    clint_base = (uint32_t *)new_space(16);
-    memset(clint_base, 0, 16);
-    
-    mtime_val = 1000;
-    mtimecmp_val = 100000;
-    
-    add_mmio_map("timer", 0xa0000048, clint_base, 16, simple_clint_handler);
-    
-    // 初始时就启用中断
-    ensure_interrupt_enabled();
-    
-    add_alarm_handle(timer_tick);
-    
-    Log("定时器初始化完成，强制启用所有中断");
+  rtc_port_base = (uint32_t *)new_space(8);
+#ifdef CONFIG_HAS_PORT_IO
+  add_pio_map ("rtc", CONFIG_RTC_PORT, rtc_port_base, 8, rtc_io_handler);
+#else
+  add_mmio_map("rtc", CONFIG_RTC_MMIO, rtc_port_base, 8, rtc_io_handler);
+#endif
+  IFNDEF(CONFIG_TARGET_AM, add_alarm_handle(timer_intr));
 }
