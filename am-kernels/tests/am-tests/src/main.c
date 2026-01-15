dcdf99365afb61607094aba6a4c7f94ece41c5ab
tracer-ysyx <tracer@ysyx.org>
2026-01-14 21:25:57 -0800

    > sim RTL ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:25:57 up  1:10,  1 user,  load average: 1.27, 0.93, 0.92

--- am-kernels/tests/am-tests/src/main.c
+++ am-kernels/tests/am-tests/src/main.c
@@ -16,7 +16,7 @@ static const char *tests[256] = {
 };
 
 int main( char *args) {
-  // args[0]= 'h';
+  args[0]= 't';
   switch (args[0]) {
     CASE('h', hello);
     CASE('i', hello_intr, IOE, CTE(simple_trap));
