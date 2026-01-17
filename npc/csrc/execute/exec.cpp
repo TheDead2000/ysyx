8ea3632e746622e7f888b32ab626540277f03089
tracer-ysyx <tracer@ysyx.org>
2026-01-16 20:46:39 -0800

    > sim RTL ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:46:39 up  1:03,  1 user,  load average: 0.95, 1.68, 1.84

--- npc/csrc/execute/exec.cpp
+++ npc/csrc/execute/exec.cpp
@@ -108,9 +108,12 @@ void init_runtime() {
   tfp = new VerilatedVcdC;
   dut->trace(tfp, 99); // 跟踪99级信号
   printf("Trace file is generated at ./build/waveform.vcd\n");
- tfp->open("./waveform.vcd");
-  // MUXDEF(CONFIG_WAVE_FORM, tfp->open("./build/waveform.vcd");
-  //        , tfp->open("/dev/null");) // 打开VCD文件
+//  tfp->open("./waveform.vcd");
+  #if CONFIG_WAVE_FORM
+   tfp->open("./build/waveform.vcd");
+  #else
+   tfp->open("/dev/null");
+  #endif // 打开VCD文件
 #ifndef NPC
   nvboard_bind_all_pins(dut);
   nvboard_init();
