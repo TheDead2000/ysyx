6a6bc35e70e803eca90cc31daf6a50308ea9d2fd
tracer-ysyx <tracer@ysyx.org>
2026-01-24 04:47:56 -0800

    > sim RTL ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  04:47:56 up  9:09,  1 user,  load average: 3.59, 1.96, 1.44

--- npc/vsrc/usr/pipeline/pipline_control.v
+++ npc/vsrc/usr/pipeline/pipline_control.v
@@ -132,8 +132,8 @@ module pipline_control (
       // 没有异常情况,正常执行
     end 
     else if (compress_stall) begin
-      _stall = 6'b000011;
-      _flush = compress_flush;
+      _stall = 6'b000010;
+      _flush = 6'b000010;
      end
     else if (csr_satp_flush_i) begin
       _stall = 6'b001111;
