9fbac7beafd6360c160ad962aae5c66554fafa9b
tracer-ysyx <tracer@ysyx.org>
2026-02-13 16:48:00 -0800

    > sim RTL ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:48:00 up 3 days, 6 min,  1 user,  load average: 2.16, 2.13, 1.36

--- npc/vsrc/usr/csr.v
+++ npc/vsrc/usr/csr.v
@@ -193,7 +193,7 @@ module CSRs(
     timehReg = 32'h0;
     instrethReg = 32'h0;
     
-    privilegeReg = 2'b11; // 初始为M模式
+    privilegeReg = 2'b10; // 初始为M模式
   end
   
   // 输出赋值
@@ -372,7 +372,7 @@ module CSRs(
       timehReg <= 32'h0;
       instrethReg <= 32'h0;
       
-      privilegeReg <= 2'b11;
+      privilegeReg <= 2'b10;
     end else begin
       // 优先处理CLINT的CSR写入请求
       if (clint_csr_write_en) begin
