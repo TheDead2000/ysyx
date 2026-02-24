239e87bd505a27b92904cd31e880d164f1238ed4
tracer-ysyx <tracer@ysyx.org>
2026-02-23 21:10:57 -0800

    > sim RTL ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:10:57 up 2 days,  3:43,  1 user,  load average: 2.47, 2.26, 5.45

--- npc/vsrc/usr/clint.v
+++ npc/vsrc/usr/clint.v
@@ -353,6 +353,7 @@ end
           };
           $display("trap_bus_i_latch[`TRAP_ECALL_M]:%h,csr_write_data_o:%h",trap_bus_i_latch[`TRAP_ECALL_M],
           {csr_mstatus_i[31:13],csr_privilege_i,csr_mstatus_i[10:8],csr_mstatus_i[3],csr_mstatus_i[6:4],1'b0,csr_mstatus_i[2:0]} );
+          trap_ecall_unstall_condition_o <= 1;
           end
           else begin
           csr_write_addr_o <= 12'h100; // sstatus
@@ -390,7 +391,7 @@ end
           };
           $display("this is call! csr_write_data_o:%h",csr_write_data_o);
         end
-
+        
         csr_state <= UPDATE_PENDING;
         $display("UPDATE_STATUS to IDLE");
       end
@@ -401,7 +402,6 @@ end
           if(trap_bus_i_latch[`TRAP_ECALL_M]) begin
             privilege_wen_o <= 1'b1;
             privilege_o <= 2'b11;
-            trap_ecall_unstall_condition_o <= 1;
           end
         end
 
