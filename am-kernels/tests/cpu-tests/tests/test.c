be881ed36cfbba0f2337be8b5c1cb3d5d877edf8
tracer-ysyx <tracer@ysyx.org>
2026-02-23 19:49:23 -0800

    > sim RTL ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:49:23 up 2 days,  2:21,  1 user,  load average: 1.98, 3.38, 2.39

--- am-kernels/tests/cpu-tests/tests/test.c
+++ am-kernels/tests/cpu-tests/tests/test.c
@@ -318,11 +318,6 @@ void s_mode_entry(void) {
     // 4. 开启MMU（SV32模式）
     uint32_t satp_value = (1 << 31) | ((uint32_t)page_table >> 12);
     csr_write(SATP, satp_value);
-    
-    asm __volatile__("mv a0,a0\n");
-        asm __volatile__("mv a0,a0\n");
-            asm __volatile__("mv a0,a0\n");
-                asm __volatile__("mv a0,a0\n");
 
     printf("S mode ecall...\n");
     ecall();
@@ -356,6 +351,7 @@ void main(void) {
     // 设置中断委托（将S模式中断委托给S模式）
     csr_write(MIDELEG, 0x222);  // 委托SSI, STI, SEI
     
+    csr_write(MCAUSE,0xB);
     // // 4. 启用中断
     // csr_set(MSTATUS, 1 << 3);   // 启用M模式中断
     // csr_set(MSTATUS, 1 << 1);   // 启用S模式中断
