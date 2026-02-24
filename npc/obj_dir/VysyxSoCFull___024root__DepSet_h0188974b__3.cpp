089c2bd455399a17d7a4ec877a8f123618ddee51
tracer-ysyx <tracer@ysyx.org>
2026-02-24 02:27:25 -0800

    > sim RTL ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  02:27:25 up 2 days,  8:59,  1 user,  load average: 2.69, 1.64, 1.99

--- npc/obj_dir/VysyxSoCFull___024root__DepSet_h0188974b__3.cpp
+++ npc/obj_dir/VysyxSoCFull___024root__DepSet_h0188974b__3.cpp
@@ -7586,7 +7586,7 @@ VL_INLINE_OPT void VysyxSoCFull___024root___nba_sequent__TOP__16(VysyxSoCFull___
                                      | (1U & vlSelf->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__top_core__DOT__rv32_csr_regfile__DOT__sstatusReg))))));
         }
         vlSelf->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__top_core__DOT__clint_u__DOT__trap_condition_latch = 0U;
-        vlSelf->__Vdly__ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__top_core__DOT__clint_u__DOT__csr_state = 8U;
+        vlSelf->__Vdly__ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__top_core__DOT__clint_u__DOT__csr_state = 9U;
     }
     vlSelf->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__top_core__DOT__clint_u__DOT__cause_value_latched 
         = vlSelf->__Vdly__ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__top_core__DOT__clint_u__DOT__cause_value_latched;
--- npc/vsrc/usr/clint.v
+++ npc/vsrc/usr/clint.v
@@ -473,7 +473,7 @@ reg trap_sret_latch;
           };
         end
         trap_condition_latch <= 0;
-        csr_state <= UPDATE_ENTRY;
+        csr_state <= CLEAR;
       end
     endcase
   end
@@ -514,7 +514,7 @@ reg trap_sret_latch;
       .id_ecall_stall_i(if_ecall_stall_i),
       // .trap_intererupt_condition_i(trap_condition_latch || trap_condition),
       .trap_ecall_unstall_condition_i(trap_ecall_unstall_condition_o),
-      
+
       .trap_mmu_page_falut(trap_mmu_page_falut),
       .csr_satp_flush_i(csr_satp_flush_i),
       .compress_stall(compress_stall),
