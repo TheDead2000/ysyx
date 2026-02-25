be7f191b762de432eb820e2fbd00f5d4aa3ad9fd
tracer-ysyx <tracer@ysyx.org>
2026-02-24 23:51:25 -0800

    > sim RTL ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:51:25 up 2 days, 19:29,  1 user,  load average: 0.77, 1.21, 2.21

--- am-kernels/tests/cpu-tests/tests/test.c
+++ am-kernels/tests/cpu-tests/tests/test.c
@@ -409,7 +409,7 @@ void s_mode_entry(void) {
     __asm__ __volatile__("mv a0,a0");
     __asm__ __volatile__("mv a0,a0");
     __asm__ __volatile__("mv a0,a0");
-    // __asm__ __volatile__("mv a0,a0");
+    __asm__ __volatile__("mv a0,a0");
    
     // __asm__ __volatile__("mv a0,a0");
     // __asm__ __volatile__("mv a0,a0");
