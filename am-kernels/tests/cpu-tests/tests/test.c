99825eb398ff1fa43bf72cf6fcc25847eff38eb7
tracer-ysyx <tracer@ysyx.org>
2026-02-24 21:00:21 -0800

    > sim RTL ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:00:21 up 2 days, 16:38,  1 user,  load average: 1.49, 1.07, 1.05

--- am-kernels/tests/cpu-tests/tests/test.c
+++ am-kernels/tests/cpu-tests/tests/test.c
@@ -392,19 +392,6 @@ void s_mode_entry(void) {
     __asm__ __volatile__("mv a0,a0");
     __asm__ __volatile__("mv a0,a0");
    
-    __asm__ __volatile__("mv a0,a0");
-    __asm__ __volatile__("mv a0,a0");
-    __asm__ __volatile__("mv a0,a0");
-    __asm__ __volatile__("mv a0,a0");
-   
-    __asm__ __volatile__("mv a0,a0");
-    __asm__ __volatile__("mv a0,a0");
-    __asm__ __volatile__("mv a0,a0");
-    __asm__ __volatile__("mv a0,a0");
-    
-    __asm__ __volatile__("mv a0,a0");
-    __asm__ __volatile__("mv a0,a0");
-    __asm__ __volatile__("mv a0,a0");
 
     // printf("S mode ecall...\n");
 
