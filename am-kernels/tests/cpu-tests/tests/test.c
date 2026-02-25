dffcd75c593e56b1440caee29444600a99076e1d
tracer-ysyx <tracer@ysyx.org>
2026-02-24 21:18:29 -0800

    > sim RTL ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:18:29 up 2 days, 16:56,  1 user,  load average: 0.90, 0.99, 1.01

--- am-kernels/tests/cpu-tests/tests/test.c
+++ am-kernels/tests/cpu-tests/tests/test.c
@@ -411,15 +411,15 @@ void s_mode_entry(void) {
     __asm__ __volatile__("mv a0,a0");
     __asm__ __volatile__("mv a0,a0");
    
-    __asm__ __volatile__("mv a0,a0");
-    __asm__ __volatile__("mv a0,a0");
-    __asm__ __volatile__("mv a0,a0");
-    __asm__ __volatile__("mv a0,a0");
+    // __asm__ __volatile__("mv a0,a0");
+    // __asm__ __volatile__("mv a0,a0");
+    // __asm__ __volatile__("mv a0,a0");
+    // __asm__ __volatile__("mv a0,a0");
    
-    __asm__ __volatile__("mv a0,a0");
-    __asm__ __volatile__("mv a0,a0");
-    __asm__ __volatile__("mv a0,a0");
-    __asm__ __volatile__("mv a0,a0");
+    // __asm__ __volatile__("mv a0,a0");
+    // __asm__ __volatile__("mv a0,a0");
+    // __asm__ __volatile__("mv a0,a0");
+    // __asm__ __volatile__("mv a0,a0");
     
 
 
