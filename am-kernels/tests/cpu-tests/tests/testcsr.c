5537f58f6938268cabcb2c7bc92011e707904539
tracer-ysyx <tracer@ysyx.org>
2026-02-26 16:12:21 -0800

    > sim RTL ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:12:21 up  2:27,  1 user,  load average: 1.21, 0.85, 0.61

--- am-kernels/tests/cpu-tests/tests/testcsr.c
+++ am-kernels/tests/cpu-tests/tests/testcsr.c
@@ -5,6 +5,8 @@ int main()
 {
 
     __asm__ volatile (
+        "li a3, 0xa0000460\n"
+        "csrw mtvec,a3\n"
         "li a2, 0xa0000830\n"
         "csrrw a2, mtvec,a2\n"        // 保存原 mtvec 到 t0
         "csrw pmpaddr0, a1\n"     // 设置 pmpaddr0 为 a1_val
