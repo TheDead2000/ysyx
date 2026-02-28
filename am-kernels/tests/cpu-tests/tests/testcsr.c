31ae9ddbcbec4cb549dd34246a7b9a811e07b506
tracer-ysyx <tracer@ysyx.org>
2026-02-27 19:41:18 -0800

    > sim RTL ysyx_22040000 张三 Linux ubuntu 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:41:18 up 20:55,  1 user,  load average: 1.45, 1.55, 0.96

--- am-kernels/tests/cpu-tests/tests/testcsr.c
+++ am-kernels/tests/cpu-tests/tests/testcsr.c
@@ -14,25 +14,25 @@ int main()
     //     :                          // 无输出
     // );
 
-    __asm__ volatile (
-        "li a3, 0xa0000460\n"
-        "mv a0,a0\n"
-        "mv a0,a0\n"
-        "csrrw a2,mscratch,a3\n"
-        "csrr tp, mscratch\n"        // 保存原 mtvec 到 t0
-        "mv a0,a0\n"
-        "sw	t0,48(tp)\n"     // 设置 pmpaddr0 为 a1_val
-        :                          // 无输出
-    );
-
     // __asm__ volatile (
     //     "li a3, 0xa0000460\n"
-    //     "csrw mtvec,a3\n"
-    //     "li a2, 0xa0000830\n"
-    //     "csrrw a2, mtvec,a2\n"        // 保存原 mtvec 到 t0
-    //     "csrw pmpaddr0, a1\n"     // 设置 pmpaddr0 为 a1_val
-    //     "csrw mtvec, a2\n"        // 恢复原 mtvec
+    //     "mv a0,a0\n"
+    //     "mv a0,a0\n"
+    //     "csrrw a2,mscratch,a3\n"
+    //     "csrr tp, mscratch\n"        // 保存原 mtvec 到 t0
+    //     "mv a0,a0\n"
+    //     "sw	t0,48(tp)\n"     // 设置 pmpaddr0 为 a1_val
     //     :                          // 无输出
     // );
+
+    __asm__ volatile (
+        "li a3, 0xa0000460\n"
+        "csrw mtvec,a3\n"
+        "li a2, 0xa0000830\n"
+        "csrrw a2, mtvec,a2\n"        // 保存原 mtvec 到 t0
+        "csrw pmpaddr0, a1\n"     // 设置 pmpaddr0 为 a1_val
+        "csrw mtvec, a2\n"        // 恢复原 mtvec
+        :                          // 无输出
+    );
 
 }
